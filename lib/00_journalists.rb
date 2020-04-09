8af6bc896f36f4b77cd7fc50f6103f49584fb0db
EtienneDesaux <62936074+EtienneDesaux@users.noreply.github.com>
2020-04-09 22:42:21 +0200

    Add files via upload
    first commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,72 @@
+journalists = Hash.new(inconnu)
+journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def number
+puts "Combien y a-t-il de journalistes dans cet array ?"
+puts "> #{array_handle.size+1}"
+puts " \n"
+end
+
+def num_handle
+puts "Combien d'handle contiennent un numéro ?"
+puts (array_handle.select{|item| item.include?("1")} + array_handle.select{|item| item.include?("2")} + array_handle.select{|item| item.include?("3")} + array_handle.select{|item| item.include?("4")} + array_handle.select{|item| item.include?("5")} + array_handle.select{|item| item.include?("6")} + array_handle.select{|item| item.include?("7")} + array_handle.select{|item| item.include?("8")} + array_handle.select{|item| item.include?("9")}).uniq.size
+puts " \n"
+end
+
+def four_letters
+puts "Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?"
+puts "> Il y en a #{}."
+puts " \n"
+end
+
+def start_maj
+puts "Combien commencent par une majuscule (première lettre juste après le @) ?"
+puts "> Il y en a #{}."
+puts " \n"
+end
+
+def maj_include
+puts "Combien contiennent au moins une majuscule ?"
+puts (array_handle.select{|item| item.include?("A")} + array_handle.select{|item| item.include?("B")} + array_handle.select{|item| item.include?("C")} + array_handle.select{|item| item.include?("D")} + array_handle.select{|item| item.include?("E")} + array_handle.select{|item| item.include?("F")} + array_handle.select{|item| item.include?("G")} + array_handle.select{|item| item.include?("H")} + array_handle.select{|item| item.include?("I")} + array_handle.select{|item| item.include?("J")} + array_handle.select{|item| item.include?("K")} + array_handle.select{|item| item.include?("L")} + array_handle.select{|item| item.include?("M")} + array_handle.select{|item| item.include?("N")} + array_handle.select{|item| item.include?("O")} + array_handle.select{|item| item.include?("P")} + array_handle.select{|item| item.include?("Q")} + array_handle.select{|item| item.include?("R")} + array_handle.select{|item| item.include?("S")} + array_handle.select{|item| item.include?("T")} + array_handle.select{|item| item.include?("U")} + array_handle.select{|item| item.include?("V")} + array_handle.select{|item| item.include?("W")} + array_handle.select{|item| item.include?("X")} + array_handle.select{|item| item.include?("Y")} + array_handle.select{|item| item.include?("Z")}).uniq.size
+puts " \n"
+end
+
+def underscore
+puts "Combien y a-t-il de underscore _ dans tous les pseudos confondus ?"
+puts "> Il y en a #{array_handle.select{|item| item.include?("_")}.size}."
+puts " \n"
+end
+# la 1e partie {array_handle.select{|item| item.include?("_")} fait apparaître tous les handles contenant un _.
+# la méthode .size calcule le nombre de handles incluant un _.
+
+def alphabetical_order
+puts "Trie la liste de handle par ordre alphabétique."
+print "> #{array_handle.sort}"
+puts " \n"
+end
+
+def shorter
+puts "Quels sont les 50 handles les plus courts de ce array ?"
+print"> #{array_handle.min(50){|a,b| a.length <=> b.length}}"
+puts " \n"
+end
+
+def position
+puts "Quelle est la position dans l'array de la personne @epenser ?"
+puts "> #{array_handle.index("@epenser")}"
+puts " \n"
+end
+
+def perform
+	number
+	num_handle
+	four_letters
+	start_maj
+	maj_include
+	underscore
+	alphabetical_order
+	shorter
+	position
+end
+
+perform
---
+++ lib/README.md
@@ -0,0 +1,13 @@
+Bonjour,
+
+Je n’avais pas besoin de m’inscrire à une session pour faire des recherches sur internet, qui donnent des résultats en anglais…
+Par contre j’avais besoin d’un cours en français pour apprendre la syntaxe du code progressivement…
+
+Je suis enseignant, et j’ai master dans ma spécialité et un master de pédagogie. Je comprends la démarche pédagogique que vous avez choisi d’adopter. Mais elle me semble mal appliquée.
+
+La question est plutôt combien me payeriez-vous pour suivre la fin de la formation ?
+Je peux aussi vous proposer la correction des fautes de frappe, de français et le signalement de ce qui ne fonctionne pas dans vos tutos…
+
+Sans rancune,
+
+Étienne
